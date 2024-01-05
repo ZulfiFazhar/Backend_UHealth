5a50a449bd3eb83dc9f07a34cf7b4152a3a3aeb0
ZulfiFazhar <zulfi.fazhar12@gmail.com>
2024-01-05 15:29:37 +0700

    change src vercell 2

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const db = require('./config/firebaseConfig');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/user', userRoutes);
+db;
 
 module.exports = app;
